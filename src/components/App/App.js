8c5e1b2e152622d87aadb571400ff525e375010c
polisen <f.westerdahl@outlook.com>
2019-08-02 13:54:31 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BussinesList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar/>
+      <BusinessList/>
+    </div>
+  );
+}
+
+export default App;
